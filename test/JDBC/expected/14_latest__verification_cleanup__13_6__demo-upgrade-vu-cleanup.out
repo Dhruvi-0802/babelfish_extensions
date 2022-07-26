02df866c94fa9dde459ccd14f9fc501928703f7b
Dhruvi Sheth <sdhruvii@amazon.com>
2022-07-26 17:28:53 +0000

    Add tests for collation, type
    Signed-off-by: Dhruvi Sheth <sdhruvii@amazon.com>

--- test/JDBC/expected/14_latest__verification_cleanup__13_6__demo-upgrade-vu-cleanup.out
+++ test/JDBC/expected/14_latest__verification_cleanup__13_6__demo-upgrade-vu-cleanup.out
@@ -1,8 +1,14 @@
-drop view demo_upgrade_view
-go
+DROP VIEW demo_upgrade_view
+GO
 
-drop table demo_upgrade_t1
-go
+DROP TABLE demo_upgrade_t1
+GO
 
-drop view demo_upgrade_view2
-go
+DROP VIEW demo_upgrade_view2
+GO
+
+DROP TABLE demo_collation
+GO
+
+DROP TYPE demo_type
+GO
--- test/JDBC/upgrade/13_6/preparation/demo-upgrade-vu-prepare.sql
+++ test/JDBC/upgrade/13_6/preparation/demo-upgrade-vu-prepare.sql
@@ -1,8 +1,19 @@
-create view demo_upgrade_view as select * from sys.objects
-go
+--view to view
+CREATE VIEW demo_upgrade_view AS SELECT * FROM sys.objects
+GO
 
-create table demo_upgrade_t1 (a fixeddecimal, b fixeddecimal, c as square(a))
-go
+--computed column to function
+CREATE TABLE demo_upgrade_t1 (a fixeddecimal, b fixeddecimal, c AS square(a))
+GO
 
-create view demo_upgrade_view2 as select a+b from demo_upgrade_t1
-go
+--computed column to operator
+CREATE VIEW demo_upgrade_view2 AS SELECT a+b FROM demo_upgrade_t1
+GO
+
+--table to collation
+CREATE TABLE demo_collation(col varchar(20) COLLATE Greek_CS_AS)
+GO
+
+--udtype to type
+CREATE TYPE demo_type FROM varchar(11) NOT NULL
+GO
